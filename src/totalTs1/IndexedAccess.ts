a7bc578432e753169f7860fbc6c80e9f8dd2196b
ndeno <ndeno@ndeno-mac.dev>
2023-03-22 20:34:04 +0000

    indexed access

---
+++ src/totalTs1/IndexedAccess.ts
@@ -0,0 +1,28 @@
+import { Equal, Expect } from './utils';
+
+export const programModeEnumMap = {
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const;
+
+/*
+Index into const object and exclude keys from resultant union
+const - indexing - indexed access
+*/
+export type IndividualProgram = Exclude<
+  typeof programModeEnumMap[keyof typeof programModeEnumMap],
+  'group' | 'announcement'
+>;
+
+type tests = [
+  Expect<
+    Equal<
+      IndividualProgram,
+      '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'
+    >
+  >
+];
