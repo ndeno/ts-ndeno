b2152bac7cad6b61ff20d28cf83368bc1d12f53d
ndeno <ndeno@users.noreply.github.com>
2024-07-15 21:41:48 +0100

    strong type library code and reduce functions

--- src/totalTs2Generics/type-args.ts
+++ src/totalTs2Generics/type-args.ts
@@ -5,3 +5,48 @@ export const createSet = <T>() => {
 export const createSetWithDefault = <T = string>() => {
   return new Set<T>();
 };
+
+export class Component<TProps> {
+  private props: TProps;
+
+  constructor(props: TProps) {
+    this.props = props;
+  }
+
+  getProps = () => this.props;
+}
+
+// adding the generic type arguement into the generic slot of the type helper
+const cloneComponent = <TProps>(component: Component<TProps>) => {
+  return new Component(component.getProps());
+};
+
+const newComponent = cloneComponent(new Component({ foo: 'bar' }));
+
+const array = [
+  {
+    name: 'John',
+  },
+  {
+    name: 'Steve',
+  },
+];
+
+// strongly type a reduce function using it's generics
+type ReducedType = Record<string, { name: string }>;
+
+const reduceWithTypeArg = array.reduce<ReducedType>((accum, item) => {
+  accum[item.name] = item;
+  return accum;
+}, {});
+
+const reduceWithTypedAccumulator = array.reduce((accum: ReducedType, item) => {
+  accum[item.name] = item;
+  return accum;
+}, {});
+
+// add a generic to prevent anys from being introduced through library code
+const fetchData = async <TData>(url: string) => {
+  const data: TData = await fetch(url).then((response) => response.json());
+  return data;
+};
