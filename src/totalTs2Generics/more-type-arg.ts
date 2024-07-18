efb8a52649c9d2daddb359903babccb707ca1ac1
ndeno <ndeno@users.noreply.github.com>
2024-07-18 21:45:08 +0100

    begin type args at different levels

---
+++ src/totalTs2Generics/more-type-arg.ts
@@ -0,0 +1,48 @@
+export const getHomePageFeatureFlags = <
+  T extends {
+    rawConfig: {
+      featureFlags: {
+        homePage: { [x: string]: boolean };
+      };
+    };
+  }
+>(
+  config: T,
+  override: (
+    flags: T['rawConfig']['featureFlags']['homePage']
+  ) => T['rawConfig']['featureFlags']['homePage']
+) => {
+  return override(config.rawConfig.featureFlags.homePage);
+};
+
+// an example of using the generic at a different slot within the arguement
+// MAKE THE GENERIC ARGUEMENT REPRESENT THE MOST LOW LEVEL THING
+export const getHomePageFeatureFlagsButElegant = <HomePageFlags>(
+  config: {
+    rawConfig: {
+      featureFlags: {
+        homePage: HomePageFlags;
+      };
+    };
+  },
+  override: (flags: HomePageFlags) => HomePageFlags
+) => {
+  return override(config.rawConfig.featureFlags.homePage);
+};
+
+const EXAMPLE = {
+  foo: 1,
+  rawConfig: {
+    featureFlags: {
+      homePage: {
+        bar: 1,
+        baz: 2,
+      },
+    },
+  },
+};
+
+const modifiedExample = getHomePageFeatureFlagsButElegant(EXAMPLE, (flags) => ({
+  ...flags,
+  more: 4,
+}));
